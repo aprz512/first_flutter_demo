dc23ea1d57102c1e072b52b7c887c309ce5b0cda
liyunlei <liyunlei@sf-express.com>
2020-09-11 14:02:14 +0800

    添加 AOP 示例

--- lib/entry/page.dart
+++ lib/entry/page.dart
@@ -11,6 +11,25 @@ class EntryPage extends Page<EntryState, Map<String, dynamic>> {
           effect: buildEffect(),
           view: buildView,
           dependencies: Dependencies<EntryState>(adapter: null, slots: <String, Dependent<EntryState>>{}),
-          middleware: <Middleware<EntryState>>[],
+          // 这里还可以传一些 middleWare，用来做 AOP，比如打印生命周期
+          effectMiddleware: <EffectMiddleware<EntryState>>[
+            pageLifecycle(),
+          ],
         );
+}
+
+EffectMiddleware<EntryState> pageLifecycle() {
+  return (
+    AbstractLogic<dynamic> logic,
+    Store<EntryState> store,
+  ) {
+    return (Effect<dynamic> effect) {
+      return (Action action, Context<dynamic> ctx) {
+        if (logic is Page<dynamic, dynamic> && action.type is Lifecycle) {
+          print('${logic.runtimeType} ${action.type.toString()} ');
+        }
+        return effect?.call(action, ctx);
+      };
+    };
+  };
 }
