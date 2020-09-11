a91404e3737005dd02e034a4fbc32435a17d52a0
liyunlei <liyunlei@sf-express.com>
2020-09-11 11:21:36 +0800

    添加 组件 注释

--- lib/third/component2/state.dart
+++ lib/third/component2/state.dart
@@ -17,6 +17,13 @@ HeaderState initState(Map<String, dynamic> args) {
   return HeaderState(count: 0);
 }
 
+// 在我们的页面设计中，数据全部都给 page 管理，component 的数据也在 page 里面
+// 我们使用 connector 将page中的数据传递给 component使用，同时也将 component更改的数据同步回page
+// connector 理解成一种数据同步手段
+// 两个泛型 T -> P，T 是大数据（页面），P是小数据（页面的组件）
+// get 是用来从大数据中获取小数据
+// set 是用来将小数据同步给大数据，保持数据同步
+// 它支持 + 操作符号，可以看重写代码
 // 从 ThirdPage 里面获取 CardPage1 的数据
 class HeaderConnector extends ConnOp<ThirdState, HeaderState>
     with ReselectMixin<ThirdState, HeaderState> {
